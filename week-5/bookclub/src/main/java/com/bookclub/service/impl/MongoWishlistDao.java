32ceb081ffa3f53766d92583ce6c738de9c0324e
mrtannguyen1 <tannguyen1@my365.bellevue.edu>
2023-10-01 23:01:21 -0400

    week-5 commit

---
+++ week-5/bookclub/src/main/java/com/bookclub/service/impl/MongoWishlistDao.java
@@ -0,0 +1,53 @@
+/*
+   Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+Additional modifications by Nguyen, T. Student 2023
+*/
+package com.bookclub.service.impl;
+
+import com.bookclub.model.WishlistItem;
+import com.bookclub.service.dao.WishlistDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("wishlistDao")
+public class MongoWishlistDao implements WishlistDao {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Override
+    public void add(WishlistItem entity) {
+        mongoTemplate.save(entity);
+    }
+
+    @Override
+    public void update(WishlistItem entity) {
+
+    }
+
+    @Override
+    public boolean remove(WishlistItem entity) {
+        Query query = new Query();
+
+        query.addCriteria(Criteria.where("id").is(entity.getId()));
+
+        mongoTemplate.remove(query, WishlistItem.class);
+
+        return true;
+    }
+
+    @Override
+    public List<WishlistItem> list() {
+        return mongoTemplate.findAll(WishlistItem.class);
+    }
+
+    @Override
+    public WishlistItem find(String key) {
+        return mongoTemplate.findById(key, WishlistItem.class);
+    }
+}
