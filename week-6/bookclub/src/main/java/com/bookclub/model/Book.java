d07db9bfc0ab00aee8032160da8c6efa41d2f032
Tan Nguyen <tannguyen@Tans-Mac-mini.local>
2023-10-07 14:12:11 -0400

    week-6 commit.

---
+++ week-6/bookclub/src/main/java/com/bookclub/model/Book.java
@@ -0,0 +1,71 @@
+/*
+   Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+Additional modifications by Nguyen, T. Student 2023
+*/
+package com.bookclub.model;
+
+import java.util.List;
+
+public class Book
+{
+    private String isbn;
+    private String title;
+    private String description;
+    private int numOfPages;
+    private List<String> authors;
+
+    public Book() {}
+
+    public Book(String isbn, String title, String description, int numOfPages, List<String> authors) {
+        this.isbn = isbn;
+        this.title = title;
+        this.description = description;
+        this.numOfPages = numOfPages;
+        this.authors = authors;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
+    }
+
+    public int getNumOfPages() {
+        return numOfPages;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book{isbn=%s, title=%s, description=%s, numOfPages=%s, authors=%s}", isbn, title, description, numOfPages, authors);
+    }
+}
---
+++ week-6/bookclub/src/main/java/com/bookclub/service/GenericCrudDao.java
@@ -0,0 +1,17 @@
+
+/*
+Krasso, R., (2021). CIS 530 Sever-Side Development. Bellevue University, all
+rights reserved. 
+Additional modifications by Nguyen, T.(2023) 
+*/
+package com.bookclub.service;
+
+import java.util.List;
+
+public interface GenericCrudDao<E, K> {
+    void add(E entity);
+    void update(E entity);
+    boolean remove(E entity);
+    List<E> list(); // Return a list of objects of type E.
+    E find(K key); // Return an object of type E by type K value.
+}
