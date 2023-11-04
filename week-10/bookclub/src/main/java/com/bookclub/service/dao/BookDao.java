664ca9bde56d71d0b8934de69fe136259972f147
Tan Nguyen <tannguyen@Tans-Mac-mini.local>
2023-11-04 11:36:13 -0400

    week-10 commit

---
+++ week-10/bookclub/src/main/java/com/bookclub/service/dao/BookDao.java
@@ -0,0 +1,11 @@
+/*
+   Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved.
+Additional modifications by Nguyen, T. Student 2023
+*/
+package com.bookclub.service.dao;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.GenericDao;
+
+public interface BookDao extends GenericDao<Book, String> {
+}
